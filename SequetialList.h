9911dfbcbcd349d4747eb71faec5ee5e06a4ccce
wick <wick.liu@outlook.com>
2020-03-23 22:08:25 +0800

    Sequence list v1.0
    Resolve #3
    
    Signed-off-by: wick <wick.liu@outlook.com>

--- SequetialList.h
+++ SequetialList.h
@@ -16,14 +16,56 @@ class SequentialList {
 
   ~SequentialList() { delete[] arr; }
 
-  bool insert(T data) {
-    if (size < length) {
-      arr[size++] = data;
-      return true;
-    }
-    return false;
+  bool IsEmpty() { return 0 == size; }
+
+  T get(int index) {
+    if (index >= size || index < 0) exit(1);
+    return arr[index];
   }
 
+  bool set(int index, T data) {
+    if (index >= size || index < 0) exit(1);
+    arr[index] = data;
+    return true;
+  }
+
+  void insert(int index, T data) {
+    if (size >= length) exit(1);
+    for (int i = size; i > index; i--) arr[i] = arr[i - 1];
+    arr[index] = data;
+    size++;
+  }
+
+  T remove(int index) {
+    if (index >= size || index < 0 || IsEmpty()) exit(1);
+
+    int tmp = arr[index];
+    for (int i = index; i < size; i++) {
+      arr[i] = arr[i + 1];
+    }
+    size--;
+    return tmp;
+  }
+
+  int removeAll(T data) {
+    int count = 0;
+    if (IsEmpty()) exit(1);
+    for (int i = 0; i < size; i++) {
+      if (arr[i] == 1) {
+        remove(i);
+        i--;
+        count++;
+      }
+    }
+    return count;
+  }
+
+  int contains(T data) {
+    int count = 0;
+    for (int i = 0; i < size; i++)
+      if (arr[i] == data) count++;
+    return count;
+  }
   void traversal(const std::function<void(const T &)> &visit) {
     for (int i = 0; i < size; i++) visit(*arr + i);
   }
--- main.cc
+++ main.cc
@@ -2,23 +2,49 @@
 #include "SequetialList.h"
 #include "gtest/gtest.h"
 
+
 TEST(Suite1, Test1)
 {
     int N = 8;
     SequentialList<int> list(N);
+    SequentialList<int> list1(N);
 
+    //test insert
     for (int i = 0; i < N; i++) {
-        list.insert(i + 1);
+        list.insert(0, i + 1);
     }
-
+    
+    for (int i = 0;i < N; i++) {
+        list1.insert(i, i + 1);
+    }
+    
     int n = 0;
     for (int d : list) {
         n++;
-        EXPECT_EQ(d, n);
+    }
+    EXPECT_EQ(n, N);
+    n = 0;
+    for (int d : list1) {
+        n++;
     }
     EXPECT_EQ(n, N);
 
-    EXPECT_EQ(list.insert(n + 1), false);
+    //test get
+    EXPECT_EQ(list.get(0), 8);
+    EXPECT_EQ(list.get(1), 7);
+    //test set
+    EXPECT_EQ(list.set(0, 5), true);
+    EXPECT_EQ(list.get(0), 5);
+
+    //test remove
+    EXPECT_EQ(list.remove(2), 6);
+    //test removeall
+    for (int d : list) {
+        std::cout << d << std::endl;
+    }
+    //std::cout << list.removeAll(5) << std::endl;
+    EXPECT_EQ(list.removeAll(1), 1);
+
 }
 
 
